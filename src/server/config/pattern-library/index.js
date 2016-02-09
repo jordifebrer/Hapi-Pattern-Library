b116e7b93a92a3bf656619e5ba9c01745856a66b
BrianDGLS <briandgls@yahoo.com>
2016-02-09 11:00:36 +0000

    Iframe now reloads when scripts and styles are updated

--- src/server/config/pattern-library/index.js
+++ src/server/config/pattern-library/index.js
@@ -3,74 +3,82 @@
 const chokidar = require('chokidar');
 
 class PatternLibrary {
-  constructor(root, emitter) {
-    this.emitter = emitter;
-    this.root = root;
+    constructor(root, emitter) {
+        this.emitter = emitter;
+        this.root = root;
 
-    this.init();
-  }
+        this.init();
+    }
 
-  component() {
-    this.components = require('./component')(this.root).data();
+    component() {
+        this.components = require('./component')(this.root).data();
 
-    return this.components;
-  }
+        return this.components;
+    }
 
-  template() {
-    this.templates = require('./template')(this.root).data();
+    template() {
+        this.templates = require('./template')(this.root).data();
 
-    return this.templates;
-  }
+        return this.templates;
+    }
 
-  pattern() {
-    this.patterns = require('./pattern')(this.root).data();
+    pattern() {
+        this.patterns = require('./pattern')(this.root).data();
 
-    return this.patterns;
-  }
+        return this.patterns;
+    }
 
-  library() {
-    this.component();
-    this.template();
-    this.pattern();
-  }
+    library() {
+        this.component();
+        this.template();
+        this.pattern();
+    }
 
-  watch() {
-    const _this = this;
-    const watcher = chokidar.watch(this.root + '/pattern-library', {
-      ignored: /[\/\\]\./,
-      persitant: true
-    });
+    watch() {
+        const _this = this;
+        const watcher = chokidar.watch(this.root + '/pattern-library', {
+            ignored: /[\/\\]\./,
+            persitant: true
+        });
 
-    const handler = path => {
-      console.log(`File | ${path} | has been changed`);
+        const handler = path => {
+            console.log(`File | ${path} | has been changed`);
 
-      _this.library();
+            _this.library();
 
-      const getName = pathToFile => {
-        const arr = pathToFile.split('/');
-        
-        return arr[arr.length - 2];
-      };
+            const getName = pathToFile => {
+                const arr = pathToFile.split('/');
+                const fileType = arr.pop().split('.')[1];
+
+                if(fileType === 'scss' || fileType === 'js') {
+                    console.log(arr[arr.length - 2]);
+                    return arr[arr.length - 2];
+                } else {
+                    console.log(arr[arr.length - 1]);
+                    return arr[arr.length - 1];
+                }
+
+            };
 
 
-      _this.emitter.emit('change', {
-        components: _this.components,
-        templates: _this.templates,
-        patterns: _this.patterns,
-        file: getName(path)
-      });
-    };
+            _this.emitter.emit('change', {
+                components: _this.components,
+                templates: _this.templates,
+                patterns: _this.patterns,
+                file: getName(path)
+            });
+        };
 
-    watcher
-      .on('change', handler);
-  }
+        watcher
+            .on('change', handler);
+    }
 
-  init() {
-    this.library();
+    init() {
+        this.library();
 
-    this.watch();
-  }
+        this.watch();
+    }
 }
 
 module.exports = (root, emitter) =>
-  new PatternLibrary(root, emitter);
+    new PatternLibrary(root, emitter);
